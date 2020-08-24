9371ce769f74bbe7fe171f3509b6c4b6596a8b1c
Luigi Russo <russo.1699981@studenti.uniroma1.it>
2020-08-24 10:43:01 +0200

    Calibre Updater Tool: new options to skip dialogs

--- calibre-updater.ps1
+++ calibre-updater.ps1
@@ -1,3 +1,9 @@
+param(
+	[int32] $win = 64,
+	[switch] $i = $false,
+	[switch] $d = $false
+)
+
 $author = 'lrusso96'
 $name = 'Calibre Updater Tool'
 $year = '2020'
@@ -5,29 +11,43 @@ $license = 'GPL'
 $version = '0.1.0'
 
 
-Write-Host "Welcome to $name v.v$version ($year)."
+Write-Host "`n`nWelcome to " -NoNewLine
+Write-Host "$name " -ForegroundColor Green -NoNewLine
+Write-Host "v$version ($year)."
 Write-Host "This script has been developed by $author and is distributed under $license license."
 
-Write-Host '`nWould you like to proceed? (Y/N): ' -ForegroundColor Yellow -NoNewline
-$Readhost = Read-Host
-Switch ($ReadHost) {
-	N {$shouldDownload=$false}
-	Default {$shouldDownload=$true}
-}
+Write-Host "`nWindows Version: " -ForegroundColor Yellow -NoNewLine
+Write-Host "$win bit"
+Write-Host 'Skip Download Dialog: ' -ForegroundColor Yellow -NoNewLine
+Write-Host "$d"
+Write-Host 'Skip Installation Dialog: ' -ForegroundColor Yellow -NoNewLine
+Write-Host "$i"
 
-If (-Not $shouldDownload) {
-	Read-Host 'Nevermind. See you! (press enter to exit)'
-	exit
+If (-Not $d){
+	Write-Host "`nWould you like to proceed? (Y/N): " -ForegroundColor Yellow -NoNewline
+	$Readhost = Read-Host
+	Switch ($ReadHost) {
+		N {$shouldDownload=$false}
+		Default {$shouldDownload=$true}
+	}
+
+	If (-Not $shouldDownload) {
+		Write-Host 'Nevermind. See you!'
+		exit
+	}
 }
 
 Invoke-WebRequest https://calibre-ebook.com/dist/win64 -O calibre_installer.msi
-Write-Host '`nWould you like to install Calibre now? (Y/N): ' -ForegroundColor Yellow -NoNewline
-$Readhost = Read-Host
-Switch ($ReadHost) {
-	N {$shouldInstall=$false}
-	Default {$shouldInstall=$true}
+
+If (-Not $i){
+	Write-Host "`nWould you like to install Calibre now? (Y/N): " -ForegroundColor Yellow -NoNewline
+	$Readhost = Read-Host
+	Switch ($ReadHost) {
+		N {$shouldInstall=$false}
+		Default {$shouldInstall=$true}
+	}
 }
 
-If ($shouldInstall) {
+If ($i -or $shouldInstall) {
 	msiexec /i 'calibre_installer.msi'
 }
