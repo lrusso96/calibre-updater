755a4a716c4f7a64b1b8ff48e3c90ced690422a2
Luigi Russo <russo.1699981@studenti.uniroma1.it>
2020-08-24 15:32:45 +0200

    Calibre Updater Tool: minor enhancements

--- calibre-updater.ps1
+++ calibre-updater.ps1
@@ -11,17 +11,21 @@ $license = 'GPL'
 $version = '0.1.0'
 
 
-Write-Host "`n`nWelcome to " -NoNewLine
-Write-Host "$name " -ForegroundColor Green -NoNewLine
-Write-Host "v$version ($year)."
-Write-Host "This script has been developed by $author and is distributed under $license license."
+function Print-Header(){
+	Write-Host "Welcome to " -NoNewLine
+	Write-Host "$name " -ForegroundColor Green -NoNewLine
+	Write-Host "v$version ($year)."
+	Write-Host "This script has been developed by $author and is distributed under $license license."
 
-Write-Host "`nWindows Version: " -ForegroundColor Yellow -NoNewLine
-Write-Host "$win bit"
-Write-Host 'Skip Download Dialog: ' -ForegroundColor Yellow -NoNewLine
-Write-Host "$d"
-Write-Host 'Skip Installation Dialog: ' -ForegroundColor Yellow -NoNewLine
-Write-Host "$i"
+	Write-Host "`nWindows Version: " -ForegroundColor Yellow -NoNewLine
+	Write-Host "$win bit"
+	Write-Host 'Skip Download Dialog: ' -ForegroundColor Yellow -NoNewLine
+	Write-Host "$d"
+	Write-Host 'Skip Installation Dialog: ' -ForegroundColor Yellow -NoNewLine
+	Write-Host "$i"
+}
+
+Print-Header
 
 If (-Not $d){
 	Write-Host "`nWould you like to proceed? (Y/N): " -ForegroundColor Yellow -NoNewline
@@ -32,12 +36,14 @@ If (-Not $d){
 	}
 
 	If (-Not $shouldDownload) {
-		Write-Host 'Nevermind. See you!'
+		Write-Host "`nNevermind . See you!"
 		exit
 	}
 }
 
+Clear-Host
 Invoke-WebRequest "https://calibre-ebook.com/dist/win$win" -O calibre_installer.msi
+Print-Header
 
 If (-Not $i){
 	Write-Host "`nWould you like to install Calibre now? (Y/N): " -ForegroundColor Yellow -NoNewline
